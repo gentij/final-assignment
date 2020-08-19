8f12754f9cf1e61764b2d0c8dc4d484af5b78b71
gentij <gentijashari5@gmail.com>
2020-08-19 10:34:10 +0200

    posting functionality added

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 //components
 import Post from './Post'
@@ -7,19 +7,59 @@ import Post from './Post'
 import Style from '../../styles/main.module.css'
 
 const Main = () => {
+    const [posts, setPosts] = useState([])
+
+    const [currentPost, setCurrentPost] = useState({
+        id: null,
+        title: '',
+        content: '',
+        upvoted: false
+    })
+
+    const updateTitle = (e) => {
+        setCurrentPost({
+                ...currentPost,
+                id: posts.length,
+                title: e.target.value
+        })
+    }
+
+    const updateContent = (e) => {
+        setCurrentPost({
+                ...currentPost,
+                id: posts.length,
+                content: e.target.value
+        })
+    }
+
+    const post = (e) => {
+        e.preventDefault();
+        if(currentPost.title && currentPost.content) {
+            setPosts([currentPost, ...posts])
+        } else {
+            alert('fields can not be empty')
+        }
+        setCurrentPost({
+            id: null,
+            title: '',
+            content: '',
+            upvoted: false
+        })
+    }
+
     return (
         <main>
             <div className={Style.container}>
                 <div className={Style.formContainer}>
-                    <form>
+                    <form onSubmit={post}>
                         <h3>New Post</h3>
                         <div className="title-input">
                             <label htmlFor="title">Title:</label>
-                            <input id="title" type="text" placeholder="Title..."/>
+                            <input id="title" value={currentPost.title} onChange={updateTitle} type="text" placeholder="Title..."/>
                         </div>
                         <div className="content-input">
                             <label htmlFor="content">Content:</label>
-                            <textarea id="content" type="text" placeholder="Content..."/>
+                            <textarea id="content" value={currentPost.content} onChange={updateContent} type="text" placeholder="Content..."/>
                         </div>
                         <button className={Style.btn}>Post</button>
                     </form>
@@ -28,11 +68,23 @@ const Main = () => {
                 <div className={Style.postsContainer}>
                     <h3 className={Style.welcome}>Welcome, here are the recent posts:</h3>
                     <div className={Style.posts}>
-                        <Post />
-                        <Post />
-                        <Post />
-                        <Post />
-                        <Post />
+                        {
+                            posts.length === 0 ? (
+                                <div className={Style.empty}>
+                                    <p>no posts yet...</p>
+                                </div>
+                            ) : (
+                                posts.map(e => (
+                                    <Post 
+                                        key = {e.id}
+                                        id = {e.id}
+                                        title={e.title}
+                                        content = {e.content}
+                                        upvoted = {e.upvoted}
+                                    />
+                                ))
+                            )
+                        }
                     </div>
                 </div>
             </div>
--- src/components/main/Post.js
+++ src/components/main/Post.js
@@ -7,22 +7,29 @@ import Style from '../../styles/post.module.css'
 import { FaTags } from 'react-icons/fa'
 import { FaTrashAlt } from 'react-icons/fa'
 import { FaArrowAltCircleUp } from 'react-icons/fa'
+import { FaArrowAltCircleDown } from 'react-icons/fa'
 
-const Post = () => {
+const Post = (props) => {
 
     return (
         <div className={Style.post}>
             <div className={Style.wrapper}>
                 <div className={Style.postHeader}>
-                    <h3 className="title">Lorem, ipsum dolor.</h3>
+                    <h3 className="title">{props.title}</h3>
                     <p className="date">18/08/2020</p>
                 </div>
                 <div className={Style.contentWrapper}>
-                    <p className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus praesentium voluptatem, quod tempora quo eius ducimus provident consectetur saepe blanditiis accusantium nemo consequuntur cupiditate dicta molestiae corrupti odit mollitia facilis quasi dolorum? Atque perspiciatis omnis explicabo hic dolor, officia porro! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium optio labore vel aliquam quaerat officiis hic doloremque inventore rerum aliquid ipsum at adipisci ab ullam corrupti molestias ipsa, fuga quidem! Dignissimos amet qui obcaecati sint, corporis enim dolore aliquam? Ab.   </p>
+                    <p className="content">{props.content}</p>
                 </div>
                 <div className={Style.interactions}>
                     <div className="rating">
-                        <button><FaArrowAltCircleUp /> Upvote</button>
+                        {
+                            props.upvoted ? (
+                                <button><FaArrowAltCircleDown /> Downvote</button>
+                            ) : (
+                                <button><FaArrowAltCircleUp /> Upvote</button>
+                            )
+                        }
                     </div>
                     <div className={Style.options}>
                         <button><FaTags /> Edit</button>
